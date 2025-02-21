6bb086e15b85496f9019c11b96db1a0b9a80f2c6
Evan Hearne <evanhearne02@gmail.com>
2025-02-21 12:00:39 +0000

    implement state in real time info page for when search bar is used

--- lib/pages/real_time_info_page.dart
+++ lib/pages/real_time_info_page.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 import '../widgets/location_display.dart';
 
-class RealTimeInfoPage extends StatelessWidget {
+class RealTimeInfoPage extends StatefulWidget {
   const RealTimeInfoPage({super.key});
 
   @override
+  _RealTimeInfoPageState createState() => _RealTimeInfoPageState();
+}
+
+class _RealTimeInfoPageState extends State<RealTimeInfoPage> {
+  String _searchQuery = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
+            padding: const EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
             child: SearchBar(
               hintText: 'Search for a bus stop...',
-              trailing: [Icon(Icons.search)],
+              trailing: const [Icon(Icons.search)],
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery = value;
+                });
+              },
             ),
           ),
           Expanded(
             child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: LocationDisplay(),
+              padding: const EdgeInsets.all(16.0),
+              child: _searchQuery.isEmpty
+                  ? const LocationDisplay()
+                  : Container(), // No content for now when search query is not empty
             ),
           ),
         ],
