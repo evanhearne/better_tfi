1a287ab1ca8987180629028b6f4d5bfb5b166490
Evan Hearne <evanhearne02@gmail.com>
2025-04-25 09:39:16 +0100

    fix android package bug

---
+++ android/app/src/main/kotlin/com/evanhearne/better_tfi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.evanhearne.better_tfi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/better_tfi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.better_tfi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
